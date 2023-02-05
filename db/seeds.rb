bfd23f72bdff1de85ce9ffafddc16539066d8dd6
santannalucas <santanna.lucas@gmail.com>
2023-02-05 20:18:30 +1100

    performance and code review

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+# Create Default Transactions Types
+TransactionType.create(name:'system')
+TransactionType.create(name:'deposit')
+TransactionType.create(name:'expenses')
+
+# Create Admin Role
+Role.create(name: 'Admin')
+Role.create(name: 'Standard Account')
+User.create(name:"Initial User", password:'Init123', email:'piggy.onrails@gmail.com', role_id:1, active: true)
+
+
+# Create Workspaces
+Workspace.create(name:'Roles')
+Workspace.create(name:'Workspaces')
+Workspace.create(name:'Rules')
+Workspace.create(name:'Custom Rules')
+Workspace.create(name:'Users')
+Workspace.create(name:'Accounts')
+Workspace.create(name:'Bank Accounts')
+Workspace.create(name:'Transactions')
+Workspace.create(name:'Currencies')
+Workspace.create(name:'Categories')
+Workspace.create(name:'Schedulers')
+Workspace.create(name:'Reports')
+
+# Rebuild Roles
+Role.rebuild
+User.first.validate_options
+Rule.where(role_id:2,workspace_id:(6..15).collect{|x| x}).each do |x| x.update(c:true,r:true,u:true, d:true, s:true, p:true) end
+
+
+
+
